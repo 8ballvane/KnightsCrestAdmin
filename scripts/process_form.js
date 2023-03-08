7b2371cf6ceba694875727921bc5b61eeb141414
Vane <vsalas@knights.ucf.edu>
2023-03-08 13:40:07 -0500

    add remove fxn

--- scripts/process_form.js
+++ scripts/process_form.js
@@ -19,6 +19,8 @@ const firebaseConfig = {
   function init() {
     document.myform.addEventListener("load", display, false);
     document.myform.addEventListener("submit", update, false);
+    document.myform.addEventListener("submit", remove, false);
+
 
     var ucfid = decodeURIComponent(window.location.search.substr(1)).split("=")[1];
     var userRef = database.ref('users').orderByChild("ucf_id").equalTo(ucfid);
@@ -80,5 +82,69 @@ const firebaseConfig = {
     
     });
   }
+
+  function remove() {
+    // Get the form elements
+  let fname;
+  let lname;
+  let id;
+  let nid;
+  let cash;
+  let library;
+  let birthday;
+  let expiration;
+  let campus;
+  let profile;
+  let caste;
+
+  
+  // Listen for the form submit event
+  document.myform.addEventListener("submit", function(event) {
+    event.preventDefault();
+  // Get the form elements
+  fname = document.getElementById("fname");
+  lname = document.getElementById("lname");
+  id = document.getElementById("id");
+  nid = document.getElementById("nid");
+  cash = document.getElementById("cash");
+  library = document.getElementById("library");
+  birthday = document.getElementById("birthday");
+  expiration = document.getElementById("expiration");
+  campus = document.getElementById("campus");
+  profile = document.getElementById("profile")
+  caste = document.querySelector('input[name="caste"]:checked').value;
+
+// Add the data to the database
+database.ref("users/").push({
+  first_name: fname.value,
+  last_name: lname.value,
+  ucf_id: id.value,
+  student_nid: nid.value,
+  knights_cash_account: cash.value,
+  library_account: library.value,
+  date_of_birth: birthday.value,
+  expiration_date: expiration.value,
+  campus: campus.value,
+  profile_picture: profile.value,
+  caste: caste,
+});
+
+ // Show a success message to the user
+ alert("Record removed successfully!");
+
+ // Reset the form fields
+ fname.value = "";
+ lname.value = "";
+ id.value = "";
+ nid.value = "";
+ cash.value = "";
+ library.value = "";
+ birthday.value = "";
+ expiration.value = "";
+ campus.value = "";
+ profile.value = "";
+});
+
+}
 
   window.addEventListener("load", init, false);
