9a0839857cdb6f4e116386c53aaf16d34f45cdcd
Vane <vsalas@knights.ucf.edu>
2023-03-10 19:13:02 -0500

    Merge branch 'main' of https://github.com/8ballvane/KnightsCrestAdmin

--- scripts/process_form.js
+++ scripts/process_form.js
@@ -1,8 +1,7 @@
 
 // Firebase configuration
-import { doc, updateDoc, deleteField, getDatabase, ref, child, get } from "firebase/firestore";
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { getDatabase, ref, child, get, update } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
 import { getDatabase } from "firebase/database";
 
 // Firebase configuration
@@ -21,8 +20,70 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
 
   // Get a reference to the database service
-  var database = firebase.database();
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+  // Get a reference to the Firebase Realtime Database
+  const dbRef = ref(database);
 
+  function init() {
+    // Get the UCF ID from the URL query parameter
+    const urlParams = new URLSearchParams(window.location.search);
+    const ucfid = urlParams.get('ucfid');
+    // Fill in the form fields with the user data
+    const firstNameField = document.getElementById('firstName');
+    const cashField = document.getElementById('cash');
+    const dobField = document.getElementById('dob');
+    const campusField = document.getElementById('campus');
+    const updateButton = document.getElementById('update');
+    const deleteButton = document.getElementById('delete');
+    const userRef = child(dbRef, `users/${ucfid}`);
+    const userSnapshot = get(userRef);
+    userSnapshot.then(snapshot => {
+      const userData = snapshot.val();
+      firstNameField.value = userData.firstName;
+      cashField.value = userData.cash;
+      dobField.value = userData.dob;
+      campusField.value = userData.campus;
+    });
+    // Add an event listener to the "Update" button
+    updateButton.addEventListener('click', () => {
+      console.log("update");
+      const newData = {
+        firstName: firstNameField.value,
+        cash: cashField.value,
+        dob: dobField.value,
+        campus: campusField.value
+      };
+      database.ref(userRef).set(newData)
+      .then(() => {
+        console.log('Record updated successfully!');
+      })
+      .catch((error) => {
+        console.error('Error updating record:', error);
+      });
+      /*
+      update(userRef, newData).then(() => {
+        alert('User data updated successfully!');
+      }).catch(error => {
+        alert(`Error updating user data: ${error.message}`);
+      });
+      */
+    });
+    
+
+    // Add an event listener to the "Delete" button
+    deleteButton.addEventListener('click', () => {
+      userRef.remove().then(() => {
+        alert('User account deleted successfully!');
+        window.location.replace('index.html');
+      }).catch(error => {
+        alert(`Error deleting user account: ${error.message}`);
+      });
+    });
+  }
+  window.addEventListener("load", init, false);
+
+  /*
   function init() {
     document.myform.addEventListener("load", display, false);
     document.myform.addEventListener("submit", update, false);
@@ -138,3 +199,4 @@ database.ref("users/").pop({
 }
 
   window.addEventListener("load", init, false);
+  */
